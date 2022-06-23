abfea634307d68dc7e5245d5ca5308b7d703bbba
Neccolini <shun11202991@gmail.com>
2022-06-23 20:32:56 +0900

    fix: seer and villager

--- utterance_generator.py
+++ utterance_generator.py
@@ -2,6 +2,7 @@ import random
 from aiwolf import (
     Role,
     GameInfo,
+    Species
 )
 
 HELLO_LIST = [
@@ -39,15 +40,20 @@ class Generator:
         if agent.game_info.day == 1:
             if talk_len < 5:
                 target = agent.game_info.divine_result.target.__str__()
-                judge = "白" if agent.game_info.divine_result.result == "HUMAN" else "黒"
+                judge = "白" if agent.game_info.divine_result.result == Species.HUMAN else "黒"
                 utter: str = "私は占い師です。占い結果は{}が{}でした".format(target, judge)
                 return utter
             elif 5 < talk_len and talk_len < 10:
                 # 投票先
                 return "投票先は~~にします" # todo 文脈理解
             else:
-                return "SKIP"
-        return "SKIP_DAY2"
+                return "Skip"
+        if agent.game_info.divine_result:
+            target = agent.game_info.divine_result.target.__str__()
+            judge: str = "白" if agent.game_info.divine_result.result == Species.HUMAN else "黒"
+            utter: str = "占い結果は{}が{}でした".format(target, judge)
+            return utter
+        return "Skip"
     def possessed_utter(self, agent):
         # 1-1 占いCO白黒, 1-2 投票先
         # 2-1 狂人CO, 2-2 投票先
@@ -70,4 +76,12 @@ class Generator:
         # 1-1 CO要求orCO対抗要求or白黒に反応* 1-2 * and 投票
         # * w,w -> グレーを疑う bがあればその人を疑う
         # 2-1
+        talk_len: int = len(agent.game_info.talk_list)
+        if agent.game_info.day == 1:
+            if talk_len < 5: # 占い師がCOしていたら
+                utter: str = "占い師〇〇把握、〇〇白把握" # NLPで解釈し、発言
+                return utter
+            else:
+                utter: str = "占い師COお願いします"
+                return utter
         return "test"
